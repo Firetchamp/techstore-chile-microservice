0ae1b055980455fb4f5b232f2af1d1c1a05fb3a7
Firetchamp <marceloalva1993@gmail.com>
2026-05-05 18:08:40 -0400

    feat: Configuración de seguridad

---
+++ api/src/main/java/cl/techstore/api/security/JwtUtil.java
@@ -0,0 +1,45 @@
+package cl.techstore.api.security;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+    // Genera una clave secreta segura para firmar el token
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    
+    // El token durará 60 minutos
+    private final int jwtExpirationMs = 3600000;
+
+    // Crea el token
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key)
+                .compact();
+    }
+
+    // Valida si el token es legítimo
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // Obtiene el nombre de usuario de adentro del token
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+}
